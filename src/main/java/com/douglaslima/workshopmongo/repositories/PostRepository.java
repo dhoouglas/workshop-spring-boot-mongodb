be5a730198a427558325d180dd4deff6b736281a
unknown <dhoouglas@hotmail.com>
2024-01-29 14:35:54 -0300

    Simple query with @Query

--- src/main/java/com/douglaslima/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/douglaslima/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.douglaslima.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.douglaslima.workshopmongo.domain.Post;
@@ -10,7 +11,12 @@ import com.douglaslima.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' }  }")
+	List<Post> searchtitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	
 
 	
 }
